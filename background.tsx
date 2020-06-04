9cd614b82ef2aa24113c6e0a5659beb6ac16a7f7
Sergio Vieira <sergiovieira@gmail.com>
2020-06-04 12:28:25 -0300

    Adds map elements

---
+++ background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tiles" tilewidth="32" tileheight="32" tilecount="28" columns="14">
+ <image source="background.png" trans="003039" width="448" height="64"/>
+</tileset>
---
+++ blocos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="blocos" tilewidth="32" tileheight="32" tilecount="60" columns="20">
+ <image source="tileset.png" trans="003039" width="640" height="96"/>
+</tileset>
